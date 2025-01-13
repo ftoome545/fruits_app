b89e20411e40a28885c0f559ffa5df95200a3eda
FatimaHure <ftoome7767@hotmail.com>
2025-01-13 22:59:16 +0300

    Add OnBoardingView and update SplashView navigation - Added `.vscode/easycode.ignore` to specify files and directories to ignore.

--- lib/core/helper_functions/on_generate_routes.dart
+++ lib/core/helper_functions/on_generate_routes.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/features/onBoarding/persentation/view/on_boarding_view.dart';
 import 'package:fruits_hub/features/splash/persentation/view/splash_view.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
     case SplashView.routeName:
       return MaterialPageRoute(builder: (context) => const SplashView());
+
+    case OnBoardingView.routeName:
+      return MaterialPageRoute(builder: (context) => const OnBoardingView());
     default:
       return MaterialPageRoute(builder: (context) => const Scaffold());
   }
