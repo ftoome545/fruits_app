2c71d62b8343ae3525ea81e500b72c50d896eccc
FatimaHure <ftoome7767@hotmail.com>
2025-01-31 15:23:12 +0300

    create the CustomAppBar and CustomTextFormField to use them in LoginViewBody

---
+++ lib/features/auth/peresntation/views/widgets/login_view_body.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub/constants.dart';
+import 'package:fruits_hub/core/widgets/custom_app_bar.dart';
+import 'package:fruits_hub/core/widgets/custom_text_form_field.dart';
+
+class LoginViewBody extends StatelessWidget {
+  const LoginViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(context: context, title: "تسجيل الدخول"),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: kHorizintalPadding),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 24,
+              ),
+              CustomTextFormField(
+                hintText: "البريد الالكتروني",
+                textInputType: TextInputType.emailAddress,
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              CustomTextFormField(
+                  suffixIcon: Icon(
+                    Icons.remove_red_eye,
+                    color: Color(0xFFC9CECF),
+                  ),
+                  hintText: "كلمة المرور",
+                  textInputType: TextInputType.visiblePassword)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
